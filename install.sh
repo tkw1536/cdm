d04cf79527fd9d866b41c14cdc7d4dc68ec8c40a
Tom Wiesing <tkw01536@gmail.com>
2016-08-07 16:48:29 +0200

    Automatically start CDM on tty1
    Install profile.sh into /etc/profile.d to automatically start it on
    login.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-install -d /usr/bin /etc /usr/share/doc/cdm/themes
+install -d /usr/bin /etc /usr/share/doc/cdm/themes /etc/profile.d/
 install -Dm755 src/cdm /usr/bin
 install -Dm644 src/cdmrc /etc/cdmrc
 install -Dm644 README.md src/profile.sh /usr/share/doc/cdm
+install -Dm755 -T src/profile.sh /etc/profile.d/zzz-cdm.sh
 install -Dm644 themes/* /usr/share/doc/cdm/themes
-
