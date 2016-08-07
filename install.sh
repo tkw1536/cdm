c887c9b2c1b7088c72f45f371e91df268405eb4c
Tom Wiesing <tkw01536@gmail.com>
2016-08-07 16:19:34 +0200

    Fix install script to setup cdm script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install -d /usr/bin /etc /usr/share/doc/cdm/themes
+install -Dm755 src/cdm /usr/bin
 install -Dm644 src/cdmrc /etc/cdmrc
 install -Dm644 README.md src/profile.sh /usr/share/doc/cdm
 install -Dm644 themes/* /usr/share/doc/cdm/themes
